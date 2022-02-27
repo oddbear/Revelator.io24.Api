fa4d31ab85050aff770e0be76e47695b33278c3a
oddbear <oddbjorn.bakke@gmail.com>
2022-02-27 16:19:36 +0100

    Added headphones command.

---
+++ Revelator.io24.Loupedeck/Commands/HeadphonesCommand.cs
@@ -0,0 +1,109 @@
+﻿using Revelator.io24.Api.Enums;
+using Revelator.io24.Api.Models.Global;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loupedeck.RevelatorIo24Plugin.Commands
+{
+    class HeadphonesCommand : PluginDynamicCommand
+    {
+        private RevelatorIo24Plugin _plugin;
+
+        public HeadphonesCommand()
+        {
+            this.AddParameter("headphonesSetMain", $"Set Headphones Main", "Headphones");
+            this.AddParameter("headphonesSetMixA", $"Set Headphones Mix A", "Headphones");
+            this.AddParameter("headphonesSetMixB", $"Set Headphones Mix B", "Headphones");
+        }
+
+        protected override bool OnLoad()
+        {
+            _plugin = (RevelatorIo24Plugin)base.Plugin;
+
+            _plugin.Device.Global.PropertyChanged += PropertyChanged;
+
+            return true;
+        }
+
+        protected override bool OnUnload()
+        {
+            _plugin.Device.Global.PropertyChanged -= PropertyChanged;
+
+            return true;
+        }
+
+        private void PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(Global.HeadphonesSource))
+                return;
+
+            base.ActionImageChanged();
+        }
+
+        protected override void RunCommand(string actionParameter)
+        {
+            if (actionParameter is null)
+                return;
+
+            var headphones = GetHeadphones(actionParameter);
+            _plugin.Device.Global.HeadphonesSource = headphones;
+        }
+
+        protected override BitmapImage GetCommandImage(string actionParameter, PluginImageSize imageSize)
+        {
+            if (actionParameter is null)
+                return base.GetCommandImage(actionParameter, imageSize);
+
+            if (_plugin.Device is null)
+                return base.GetCommandImage(actionParameter, imageSize);
+
+            var global = _plugin.Device.Global;
+            var headphones = GetHeadphones(actionParameter);
+
+            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            {
+                bitmapBuilder.Clear(BitmapColor.Black);
+
+                var path = global.HeadphonesSource == headphones
+                    ? $"Loupedeck.RevelatorIo24Plugin.Resources.Plugin.headphones_on-80.png"
+                    : $"Loupedeck.RevelatorIo24Plugin.Resources.Plugin.headphones_off-80.png";
+
+                var background = EmbeddedResources.ReadImage(path);
+                bitmapBuilder.SetBackgroundImage(background);
+
+                switch (headphones)
+                {
+                    case Headphones.Main:
+                        bitmapBuilder.DrawText("Main", 0, 60, 80, 0);
+                        break;
+                    case Headphones.MixA:
+                        bitmapBuilder.DrawText("Mix A", 0, 60, 80, 0);
+                        break;
+                    case Headphones.MixB:
+                        bitmapBuilder.DrawText("Mix B", 0, 60, 80, 0);
+                        break;
+                }
+
+                return bitmapBuilder.ToImage();
+            }
+        }
+
+        private Headphones GetHeadphones(string actionParameter)
+        {
+            switch (actionParameter)
+            {
+                case "headphonesSetMixA":
+                    return Headphones.MixA;
+                case "headphonesSetMixB":
+                    return Headphones.MixB;
+                case "headphonesSetMain":
+                default:
+                    return Headphones.Main;
+            }
+        }
+    }
+}
