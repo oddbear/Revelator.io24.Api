c9ad36f796e46b94adf56e629af29321b0311cdf
oddbear <oddbjorn.bakke@gmail.com>
2022-05-09 23:54:24 +0200

    Added simple toggle monitor out Action.

--- Revelator.io24.Loupedeck/Properties/AssemblyInfo.cs
+++ Revelator.io24.Loupedeck/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Revelator io24")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Revelator")]
+[assembly: AssemblyDescription("A plugin for the Revelator io24 audio interface.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LoupeDeck Oy")]
-[assembly: AssemblyProduct("Loupedeck2")]
-[assembly: AssemblyCopyright("Copyright (c) 2018 LoupeDeck Oy. All rights reserved.")]
+[assembly: AssemblyCompany("oddbear")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("4.3.0.7478")]
-[assembly: AssemblyVersion("4.3.0.7478")]
+[assembly: AssemblyVersion("1.0.0")]
---
+++ Revelator.io24.StreamDeck/Settings/ToggleMonitorSettings.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Revelator.io24.StreamDeck.Actions
+{
+    public class ToggleMonitorSettings
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty(PropertyName = "value")]
+        public MonitorVolumeLevel Value { get; set; } = MonitorVolumeLevel.dB_0;
+    }
+
+    public enum MonitorVolumeLevel
+    {
+        //Right number is "percentage on wheel" (as I don't have the dB calculation):
+        dB_0 = 100,
+        db_5 = 64,
+        db_10 = 50,
+        db_15 = 41
+    }
+}
