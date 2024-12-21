2054079ecaebbb0202b99590fad6a4fa60ebf71e
oddbear <oddbjorn.bakke@gmail.com>
2024-12-21 22:40:14 +0100

    Added Volume, Phones, Blend Action.

---
+++ Revelator.io24.StreamDeck/Settings/OutputLevelSettings.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using Revelator.io24.Api.Enums;
+
+namespace Revelator.io24.StreamDeck.Settings;
+
+internal class OutputLevelSettings
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonProperty(PropertyName = "outputValue")]
+    public DeviceOut DeviceOut { get; set; } = DeviceOut.MainOut;
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonProperty(PropertyName = "changeType")]
+    public VolumeType ChangeType { get; set; } = VolumeType.Absolute;
+
+    [JsonProperty(PropertyName = "value")]
+    public int Value { get; set; } = 0;
+}
